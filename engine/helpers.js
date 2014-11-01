dd654c6bb834b782120a8f94ba782eaa044d5827
Jonatan <jonatanbrown@gmail.com>
2014-11-01 22:29:24 +0100

    Fixed validation errors.

--- engine/helpers.js
+++ engine/helpers.js
@@ -179,7 +179,7 @@
     GameCreator.helpers.parseRange = function(string) {
         var i, range;
         if (string.indexOf(':') > -1) {
-            if (!(/^\d+:\d+$/.test(string)) ) {
+            if (!(/^\-?\d+:\-?\d+$/.test(string)) ) {
                 throw '"' + string + '" is not a valid range. Should be "&lt;num&gt;"" or "&lt;num&gt;:&lt;num&gt;"';
             } 
             range = string.split(":", 2);
@@ -246,7 +246,7 @@
     };
 
     GameCreator.helpers.parseNumber = function(value) {
-        if (!(/^\d+$/.test(value))) {
+        if (!(/^\-?\d+$/.test(value))) {
             throw "'" + value + "' is not a number";
         }
         return parseInt(value);
