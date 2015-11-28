87dbc6a72eeec0f9abead5715b2b5f894ead2fb9
Jake <jacobo.strm@gmail.com>
2015-11-28 16:55:45 +0100

    Updated link message

--- startup.js
+++ startup.js
@@ -113,7 +113,7 @@ $(document).ready(function() {
     //Setup sharing link button
     $("#get-url-button").on("click", function(evt){
         var path = $(evt.target).data("href");
-        prompt("Here is the direct link to your game, don't forget to make it public to make it playable by others.", path);
+        prompt("Here is the direct link to your game, don't forget to make it 'Unlisted' or 'Published' to make it playable by people with the link.", path);
     });
 });
     
